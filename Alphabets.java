75a55c3efb9ccac66cf6d239d15c9a45a36ba757
Sivaprasath123 <vsivaprasath95@gmail.com>
2020-05-16 13:08:50 +0530

    added HelloWorld.java

---
+++ Alphabets.java
@@ -0,0 +1,10 @@
+class Alphabets
+{
+  public static void main(String args[])
+  {
+    char t;
+ 
+    for (t = 'a'; t <= 'z'; t++) // For upper case use 'A' and 'Z'
+      System.out.println(t);
+   }
+}
---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+class ForLoop {
+  public static void main(String[] args) {
+    int c;
+   
+    for (c = 1; c <= 10; c++) {
+      System.out.println(c);
+    }
+  }
+}
